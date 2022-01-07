614b4af790871824e7b2d55741ae7dfd0961c564
Majdkhodari <majdkhudary@gmail.com>
2022-01-07 19:23:33 +0200

    Nested If didn't work, but here you go :D

--- index.js
+++ index.js
@@ -8,4 +8,33 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+  const BMI = (weight /(height/100)**2)
+  let bmiStatus;
+  
+        if (BMI < 18.5) {
+        bmiStatus = "You are underweight"
+      }
+      else if (BMI < 24.9) {
+       bmiStatus = "You are healthy weight"
+      }
+      else if (BMI >25 || BMI <29.9) {
+        bmiStatus = "You are overweight"
+      }
+      else { bmiStatus =  "You are Above Obesity"
+      }
+
+  
+
+  alert (`weight: ${weight}
+ age: ${age}
+ height: ${height}
+ Your BMI is ${BMI} ${bmiStatus}
+`);
+
+  
+   
+
+  
+  
+
 }
